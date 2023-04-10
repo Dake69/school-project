6d4448340fc000c54243bf66ac559b9649e9f774
Dake69 <brawlplayer914@gmail.com>
2023-04-10 11:03:06 +0300

    school project html

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, '')
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h4>qwe</h4>")
+    return render(request, 'main/about.html')
